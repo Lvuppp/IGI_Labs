c1316e825e0f89a4a649c470121300f26f4c4bd6
Lvuppp <egor.bagrovets.eb@gmail.com>
2023-02-25 21:12:47 +0300

    add the cretion of an even number list

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,3 +4,7 @@ print("Hello world!\n")
 
 print(utilities.make_math_operation(2, 4, "add"))
 
+list_of_numbers = [1, 2, 3, 4, 5, 6, 11]
+list_of_even_numbers = [x for x in list_of_numbers if x % 2 == 0]
+
+print(list_of_even_numbers)
